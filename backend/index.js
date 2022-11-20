e7a9381738a1572bd92ecd51fde8b6828d80dc64
Mark <markloercher@gmail.com>
2022-11-19 21:26:06 -0500

    added express backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+
+const app= express()
+
+
+// configuring app to read JSON (middleware function)
+app.use(express.json())
+// allows access to nod.js api from react app
+app.use(cors())
+
+
+// req is what is inputted from front end, res is what is recieved from api
+app.get('/', (req, res) => {
+    res.send('Welcome to online shop...')
+})
+
+
+// selecting PORT to run server--necessary for deployment
+const PORT = process.env.PORT || 5000
+app.listen(5000, console.log(`server running on port ${PORT}`))
