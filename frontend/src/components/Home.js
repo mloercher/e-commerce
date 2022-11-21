afda8691c421e7bf20b9b13b6bc36c27e43c9060
Mark <markloercher@gmail.com>
2022-11-21 14:13:44 -0500

    rendering products on home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,9 +2,27 @@ import React from 'react'
 import { useGetAllProductsQuery } from '../features/productsApi';
 
 function Home() {
-    const {data, error, isLoading} = useGetAllProductsQuery()
+    const { data, error, isLoading } = useGetAllProductsQuery()
     return (
-        <h1>Home</h1>
+        <div className='home-container'>
+            {isLoading ? (<p>Loading...</p>) : error ? (<p>An Error Occurred...</p>)
+                : (
+                    <>
+                        <h2>New Arrivals</h2>
+                        <div className='products'>
+                            {data?.map(product => <div key={product.id} className='product'>
+                            <h3 className='product-name'>{product.name}</h3>
+                            <img src={product.img} alt={product.name} />
+                            <div className='details'>
+                                <span >{product.desc}</span>
+                                <span className='price'>${product.price}</span>
+                            </div>
+                            <button>Add to Cart</button>
+                            </div>)}
+                        </div>
+                    </>
+                )}
+        </div>
     )
 }
 
